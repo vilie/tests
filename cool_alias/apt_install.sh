7956e60b4dcec05ba6d87573cd796b787459d502
Valentin Ilie <eminemix@gmail.com>
2014-05-26 23:18:22 +0300

    Add apt_install, video_install.
    Signed-off-by: Valentin Ilie <eminemix@gmail.com>

---
+++ cool_alias/apt_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt-get update
+apt-get install vim git preload htop xchat lm-sensors pepperflashplugin-nonfree chromium-browser openssh-server ubuntu-restricted-extras
---
+++ cool_alias/video_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt-get update
+apt-get install bumblebee bumblebee-nvidia primus nvidia-331
